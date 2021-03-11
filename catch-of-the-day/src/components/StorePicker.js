918210e5f15c1487a4c03545a24200faa5b94a03
dave <dave@davetyler.net>
2021-03-10 20:15:31 -0700

    added css to react project

--- catch-of-the-day/src/components/StorePicker.js
+++ catch-of-the-day/src/components/StorePicker.js
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 class StorePicker extends React.Component{
     render(){
-        return <p>I am the Store picker!</p>
+        // return <p>I am the Store picker!</p>
+        //JSX
+        // return React.createElement('p', { className: 'hey'}, 'REACT NSX');
+        return (
+        <Fragment>
+        { /*Thi is a JSX comment */ }
+            <p>Fish</p>
+            <form className='store-selector'>
+                <h2>Please enter a store</h2>
+                <input type="text" required placeholder="Store Name"/>
+                <button type="submit">Visit Store</button>
+            </form>
+        </Fragment>
+        );
     }
 }
 
