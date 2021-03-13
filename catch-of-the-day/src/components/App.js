34a5e2df86d5ba3a403c24f9bcedb6c555ea5a4a
Dave <dave@davetyler.net>
2021-03-12 19:28:40 -0700

    updating code

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -12,8 +12,6 @@ class App extends React.Component {
                 </div>
                 <Order />
                 <Inventory />
-                
-                
             </div>
         )
     }
